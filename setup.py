bb780190950380152e28401b94a342b044a9b415
Peter Bittner <django@bittner.it>
2018-10-21 15:56:08 +0200

    Test against Django 2.1 and Python 3.7
    Drop Python 3.3 and Django 1.8 through 1.10
    
    Simplify packaging implementation

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
+#!/usr/bin/env python3
+"""
+Packaging setup of Django integration for behave.
+"""
 from os import chdir
-from os.path import abspath, dirname, join, normpath
+from os.path import abspath, dirname, normpath
 from setuptools import find_packages, setup
-
-
-def read_file(filename):
-    with open(join(dirname(abspath(__file__)), filename)) as f:
-        return f.read()
-
 
 # allow setup.py to be run from any path
 chdir(normpath(abspath(dirname(__file__))))
 
-import behave_django  # noqa
+import behave_django as package  # noqa
+
+
+def read_file(filename):
+    with open(filename) as file:
+        return file.read()
+
 
 setup(
     name='behave-django',
-    version=behave_django.__version__,
+    version=package.__version__,
+    license=package.__license__,
+    description=package.__doc__.strip(),
+    long_description=read_file('README.rst'),
     packages=find_packages(exclude=['test*']),
     include_package_data=True,
-    license=behave_django.__license__,
-    description=behave_django.__doc__.strip(),
-    long_description=read_file('README.rst'),
-    url='https://github.com/behave/behave-django',
-    author='Mitchel Cabuloy',
-    author_email='mixxorz@gmail.com',
-    maintainer='Mitchel Cabuloy, Peter Bittner',
-    maintainer_email='mixxorz@gmail.com',
+    url=package.__url__,
+    author=package.__author__,
+    author_email=package.__email__,
+    maintainer=package.__maintainer__,
+    maintainer_email=package.__email__,
     install_requires=read_file('requirements.txt'),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -33,11 +37,9 @@ setup(
         'Environment :: Plugins',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
@@ -45,10 +47,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Testing',
